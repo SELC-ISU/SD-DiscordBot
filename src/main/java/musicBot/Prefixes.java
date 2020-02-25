7936ca4a38b671e3bb6f3ef3cbba1f61ba448f85
EdDao277 <60050297+EdDao277@users.noreply.github.com>
2020-02-25 09:28:55 -0600

    Made a change to variables

--- src/main/java/musicBot/Prefixes.java
+++ src/main/java/musicBot/Prefixes.java
@@ -3,9 +3,11 @@ package musicBot;
 import java.util.HashMap;
 import java.util.Map;
 
+import SD.Discord.Bot.Variables;
+
 public class Prefixes 
 {
-	public static final String PREFIX = "!";
+	public static final String PREFIX = Variables.getPrefix();
     public static final long OWNER = 191231307290771456L;
     public static final Map<Long, String> PREFIXES = new HashMap<>();
 }
